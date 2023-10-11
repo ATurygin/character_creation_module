c9a4aa4626973ec7742c2d85ec4a73ad4166add9
Александр Турыгин <turygin@list.ru>
2023-10-11 22:25:54 +0400

    add docstrings for all functions

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name: str,
            char_class: str) -> str:
+    """Implement the attack mechanic."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику'
                 f' равный {5 + randint(3, 5)}')
@@ -19,6 +20,7 @@ def attack(char_name: str,
 
 def defence(char_name: str,
             char_class: str) -> str:
+    """Implement the defence mechanic."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -30,6 +32,7 @@ def defence(char_name: str,
 
 def special(char_name: str,
             char_class: str) -> str:
+    """Implement the use of special abilities."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное'
                 f' умение «Выносливость {80 + 25}»')
@@ -44,6 +47,7 @@ def special(char_name: str,
 
 def start_training(char_name: str,
                    char_class: str) -> str:
+    """Demonstrate the use of controls."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -68,6 +72,11 @@ def start_training(char_name: str,
 
 
 def choice_char_class() -> str:
+    """Choose a character class.
+
+    Promts the user with available classes,
+    reads user input and returns the choice.
+    """
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
